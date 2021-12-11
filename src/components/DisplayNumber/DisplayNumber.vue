6e612a8a06c4a73929cab1de2d7c2f75597bbc8e
Florin Codreanu <florin.codreanu@gmail.com>
2021-12-11 23:39:57 +0200

    change font

--- src/components/DisplayNumber/DisplayNumber.vue
+++ src/components/DisplayNumber/DisplayNumber.vue
@@ -9,15 +9,15 @@
 @import "../../assets/mixins";
 .span__input{
   @include media_small {
-   font-size:0.9rem;
+   font-size:1rem;
   }
 
   @include media_medium {
-    font-size:1rem;
+    font-size:1.2rem;
   }
 
   @include media_large {
-    font-size:1.1rem;
+    font-size:1.4rem;
   }
 }
 </style>
